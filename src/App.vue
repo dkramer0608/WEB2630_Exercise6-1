8effacbaa01e93478bc365628f335831e799cdc1
Danielle <daniellekramer@mail.weber.edu>
2022-03-28 08:11:19 -0600

    final

--- src/App.vue
+++ src/App.vue
@@ -21,19 +21,13 @@
 <div>
     <h1>{{ name| uppercase }}</h1>
   </div>
-
-    	<div>
-    First 4: {{ message | first4Chars}} <br>
-		Last 4: {{ message | last4Chars }}
-	</div>    
+ 
 
         <div id="example">
   <p>Original message: "{{ message }}"</p>
   <p>Computed reversed message: "{{ reversedMessage }}"</p>
 </div>
 
-        <!-- ToDo 5 -->
-        <!-- Share the Computed Property rebuilding ToDo 4 via a Mixin -->
 
       </div>
     </div>
@@ -48,11 +42,6 @@ export default {
     };
   },
 
-  data: function() {
-    return {
-      name: "Danielle Kramer"
-    };
-  },
   filters: {
     uppercase: function(value) {
       return value.toUpperCase();
@@ -60,12 +49,6 @@ export default {
   }
 };
 
-
-Vue.filter("first4Chars", str => str.substring(0, 4))
-Vue.filter("last4Chars", str => str.substring(str.length - 4))
-
-import Vue from 'vue'
-import App from './main'
 
 new Vue({
   el: '#main',
@@ -87,16 +70,6 @@ var vm = new Vue({
 })
 
 
-const helloMixin = {
-  created() {
-    this.hello();
-  },
-  methods: {
-    hello() {
-      alert("Danielle Kramer");
-    }
-  }
-};
 
 new Vue({
   el: "#app",
