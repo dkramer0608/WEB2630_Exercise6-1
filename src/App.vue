700d41aa6518e8a61f0d6d1d3947e3122eb6280a
Danielle <daniellekramer@mail.weber.edu>
2022-03-28 15:20:27 -0600

    final

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <template>
-  <div class="container" id="app">
+  <div class="container">
     <div class="row">
-      <div class="col">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Directives</h1>
+        <!-- ToDo 1 -->
+        <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
+
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Filters & Mixins</h1>
         <input type="text" class="form-control" v-model="text"/>
         <p>{{text|reverseText}}</p>
@@ -23,6 +31,7 @@
 <script>
 
 import {countMixin} from "./main.js"; 
+
 export default {
   name: "App",
   data() {
